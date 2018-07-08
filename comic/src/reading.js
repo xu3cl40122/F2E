718ca0b0ccc30b5e8674ed9a88af442d20cc4bd0
xu3cl40122 <xu3cl40122@gmail.com>
2018-07-08 22:18:27 +0800

    add detect scroll

--- comic/src/reading.js
+++ comic/src/reading.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
+import LazyLoad from 'react-lazyload'
 import comicData from './data'
+import comic_data from './data';
 export class Reading extends React.Component {
     constructor(props){
         super(props)
         this.state = {
             comicData : comicData,
             indexNow:0
+
         }
         this.setIndex = this.setIndex.bind(this)
         this.selectChange = this.selectChange.bind(this)
+        this.showChangeChap = this.showChangeChap.bind(this)
     }
     setIndex(indexToSet){
         const{indexNow} = this.state
@@ -38,9 +42,14 @@ export class Reading extends React.Component {
             [e.target.name]:e.target.value
         })
     }
-    
+    showChangeChap(prev,next){
+        this.setState({
+            showPrev:prev,
+            showNext:next
+        })
+    }
     render() {
-        const{comicData, indexNow} = this.state
+        const{comicData, indexNow,showNext,showPrev} = this.state
         return (
             <div className='mainContainer'>
                 <div className='reading'>
@@ -76,12 +85,13 @@ export class Reading extends React.Component {
                         </div>
                     </div>
                     <div className='pageSelector'>
-                        <div className='page_dark_left'></div>
-                        <div className='page_dark_right'></div>
+                        {(indexNow == 0) | (showPrev) ? <div className='page_dark_prevChap' data-text='prev'><i className='fa fa-angle-double-left'></i><div className='test'></div></div> : <div className='page_dark_left'></div>}
+                        {indexNow == (comicData.length - 1) | showNext ? <div className='page_dark_nextChap' data-text='next'><i className='fa fa-angle-double-right'></i></div> : <div className='page_dark_right'></div>}
                         <PageRow 
                         setIndex = {this.setIndex}
                         comicData = {comicData}
                         indexNow = {indexNow}
+                        showChangeChap = {this.showChangeChap}
                         />
                     </div>
                 </div>
@@ -89,7 +99,81 @@ export class Reading extends React.Component {
         )
     }
 }
+class PageRow extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            scrollLeft: 0
+        }
+        this.setScrollPosition = this.setScrollPosition.bind(this)
+        this.handleScroll = this.handleScroll.bind(this)
+    }
+    setScrollPosition(left) {
+        this.setState({
+            scrollLeft: left
+        })
+    }
+    componentDidMount() {
+        // get dom size and position
+        var rect = ReactDOM.findDOMNode(this)
+            .getBoundingClientRect()
+        this.setState({
+            rowLeft: rect.left,
+            rowWidth: rect.width
+        })
+        // 綁定偵測捲動事件
+        var scrollBody = ReactDOM.findDOMNode(this)
+        scrollBody.addEventListener('scroll', this.handleScroll)
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.scrollLeft != this.state.scrollLeft) {
+            ReactDOM.findDOMNode(this).scrollLeft += this.state.scrollLeft
+        }
+    }
+    handleScroll() {
+        const{showChangeChap} = this.props
+        var scrollBody = ReactDOM.findDOMNode(this)
+        //console.log(scrollBody.scrollWidth - scrollBody.clientWidth, scrollBody.scrollLeft)
+        //                總寬                       卷軸本身寬                  卷軸最左邊位置
+        if ((scrollBody.scrollWidth - scrollBody.clientWidth) == scrollBody.scrollLeft){
+            var scrolltoRightEnd = true
+        }
+        if (scrollBody.scrollLeft == 0 & scrolltoRightEnd) {
+            showChangeChap(true,true)
+        }
+        else if (scrollBody.scrollLeft == 0 ) {
+            showChangeChap(true,false)
+        }
+        else if (scrolltoRightEnd){
+            showChangeChap(false,true)
+        }
+        else{
+            showChangeChap(false,false)
+        }
 
+    }
+    render() {
+        const { comicData, indexNow, setIndex } = this.props
+        return (
+            <div className='page_row'>
+                {comicData.map((page, index) => {
+                    return (
+                        <PageCol
+                            page={page}
+                            index={index}
+                            indexNow={indexNow}
+                            setIndex={setIndex}
+                            key={index}
+                            rowLeft={this.state.rowLeft}
+                            rowWidth={this.state.rowWidth}
+                            setScrollPosition={this.setScrollPosition}
+                        />
+                    )
+                })}
+            </div>
+        )
+    }
+}
 class PageCol extends React.Component{
     constructor(props){
         super(props)
@@ -109,61 +193,13 @@ class PageCol extends React.Component{
     render(){
         const{index,indexNow,page} = this.props
         return(
+            <LazyLoad height='163px'>
             <div className={index == indexNow ? 'page_col page_col-active' : 'page_col'} onClick={this.setIndex}>
                 <p>{index + 1}</p>
                 <img src={'../pic/' + page} alt="" />
             </div>
+            </LazyLoad>
         )
     }
 }
 
-class PageRow extends React.Component{
-    constructor(props){
-        super(props)
-        this.state={
-            scrollLeft:0
-        }
-        this.setScrollPosition = this.setScrollPosition.bind(this)
-    }
-    setScrollPosition(left){
-        this.setState({
-            scrollLeft:left
-        })
-    }
-    componentDidMount(){
-        // get dom size and position
-        var rect = ReactDOM.findDOMNode(this)
-            .getBoundingClientRect()
-        this.setState({
-            rowLeft : rect.left,
-            rowWidth: rect.width
-        })
-    }
-    componentDidUpdate(prevProps,prevState){
-        if(prevState.scrollLeft != this.state.scrollLeft){
-            ReactDOM.findDOMNode(this).scrollLeft += this.state.scrollLeft
-        }
-    }
-    
-    render(){
-        const{comicData,indexNow,setIndex} = this.props
-        return(
-            <div className='page_row'>
-                {comicData.map((page, index) => {
-                    return (
-                        <PageCol
-                            page={page}
-                            index={index}
-                            indexNow={indexNow}
-                            setIndex={setIndex}
-                            key={index}
-                            rowLeft={this.state.rowLeft}
-                            rowWidth = {this.state.rowWidth}
-                            setScrollPosition = {this.setScrollPosition}
-                        />
-                    )
-                })}
-            </div>
-        )
-    }
-}
