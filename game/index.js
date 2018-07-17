e7703ff19d93448d0ed1861bddd9e7c6e9fd107f
xu3cl40122 <xu3cl40122@gmail.com>
2018-07-17 22:41:51 +0800

    can trace mouse

--- game/index.js
+++ game/index.js
@@ -11,7 +11,7 @@ app.renderer.view.style.display = "block";
 app.renderer.autoResize = true;
 app.renderer.resize(window.innerWidth, window.innerHeight);
 
-var player = PIXI.Sprite.fromImage('./pic/body.png')
+var player = PIXI.Sprite.fromImage('./pic/body - 3.png')
 player.anchor.set(0.5); //讓定位點置中
 // move the sprite to the center of the screen
 player.x = app.screen.width / 2;
@@ -19,3 +19,25 @@ player.y = app.screen.height / 2;
 
 app.stage.addChild(player);
 app.stage.interactive = true
+
+function rotateToPoint(mx, my, px, py) {
+    var self = this;
+    var dist_Y = my - py;
+    var dist_X = mx - px;
+    var angle = Math.atan2(dist_Y, dist_X);
+    //var degrees = angle * 180/ Math.PI;
+    return angle;
+}
+// start animating
+animate();  
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    // just for fun, let's rotate mr rabbit a little
+    player.rotation = rotateToPoint(app.renderer.plugins.interaction.mouse.global.x, app.renderer.plugins.interaction.mouse.global.y, player.position.x, player.position.y);
+
+    
+    // render the container
+    app.renderer.render(app.stage);
+}
