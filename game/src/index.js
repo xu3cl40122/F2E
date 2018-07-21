f416e9f7398ba7e1723934a1e6c4f0d21fc5dc0c
xu3cl40122 <xu3cl40122@gmail.com>
2018-07-21 14:05:05 +0800

    can create wave 1

--- game/src/index.js
+++ game/src/index.js
@@ -12,7 +12,7 @@ app.renderer.autoResize = true;
 app.renderer.resize(window.innerWidth, window.innerHeight);
 
 // init player
-var player = PIXI.Sprite.fromImage('./pic/body - 3.png')
+var player = PIXI.Sprite.fromImage('./pic/player3.png')
 player.anchor.x = 0.5
 player.anchor.y = 0.5//讓定位點置中
 // move the sprite to the center of the screen
@@ -26,72 +26,89 @@ var distance = 50 // 發射子彈位置的旋轉半徑
 var offset={x:0,y:0}
 
 // init shield
-var shield = new PIXI.Sprite.fromImage('./pic/shield.png')
+var shield = new PIXI.Sprite.fromImage('./pic/shield2.png')
 shield.anchor.set(0.5)
 shield.updatePosition = ()=>{
     shield.rotation = player.rotation
-    shield.x = player.position.x - Math.cos(player.rotation) * 40 + offset.x
-    shield.y = player.position.y - Math.sin(player.rotation) * 40 + offset.y
+    shield.x = player.position.x - Math.cos(player.rotation) * 40 
+    shield.y = player.position.y - Math.sin(player.rotation) * 40 
 } 
 app.stage.addChild(shield)
+// init gun 
+var gun = new PIXI.Sprite.fromImage('./pic/gun.png')
+gun.anchor.set(0.5)
+gun.updatePosition=()=>{
+    gun.rotation = player.rotation
+    gun.x = player.position.x + Math.cos(player.rotation) * 35
+    gun.y = player.position.y + Math.sin(player.rotation) * 35
+}
+app.stage.addChild(gun)
 
-var graphics = new PIXI.Graphics();
-graphics.lineStyle(0);
-graphics.beginFill(0xFFFF0B, 0.5);
-graphics.drawRect(50, 250, 120, 120);
-graphics.endFill();
 
 //var monster = new PIXI.Sprite(graphics.generateTexture()) 用graphic 產生sprite
-var monster = new PIXI.Sprite.fromImage('./pic/circle.png')
-monster.anchor.set(0.5)
-monster.x = 200
-monster.y = 200 
-var monsterDistance = 300
- 
-monster.updatePosition = ()=>{
-    monster.rotation += 0.01
-    // 轉一圈就歸零
-    if (monster.rotation > Math.PI * 2 ){
-        monster.rotation = 0
-    }
-    monster.x = player.position.x - Math.cos(monster.rotation) * monsterDistance
-    monster.y = player.position.y - Math.sin(monster.rotation) * monsterDistance
-}
-app.stage.addChild(monster)
-
+var enemyWaves = [3,4,5]
+var livingEnemys = []
 var bullets = [];
-var bulletSpeed = 5;
-var monsterBullets = []
+var enemyBullets = []
+
+for(let w = 0; w < enemyWaves[0]; w++){
+    createCircleEnemy()
+}
 app.stage.on("mousedown", function (e) {
     shoot(player.rotation, {
         x: player.position.x + Math.cos(player.rotation) * distance + offset.x,
         y: player.position.y + Math.sin(player.rotation) * distance + offset.y
-    });
-})
-app.stage.on("mousedown", function (e) {
-    monsterShoot(monster.rotation, {
-        x: monster.position.x + Math.cos(monster.rotation) * 30,
-        y: monster.position.y + Math.sin(monster.rotation) * 30
-    })
+    },'./pic/bullet.png',bullets);
 })
 
-function shoot(rotation, startPosition) {
-    var bullet = new PIXI.Sprite.fromImage('./pic/bullet.png');
+app.stage.on("mousedown", function (e) {
+    for(let e in livingEnemys){
+        shoot(livingEnemys[e].rotation, {
+            x: livingEnemys[e].position.x + Math.cos(livingEnemys[e].rotation) * 30,
+            y: livingEnemys[e].position.y + Math.sin(livingEnemys[e].rotation) * 30
+        }, './pic/c_bullet.png', enemyBullets)
+    }
+    
+})
+
+
+// start animating
+animate();
+app.ticker.add(delta => animate(delta))
+function animate(delta) {
+    //requestAnimationFrame(animate);
+    // just for fun, let's rotate mr rabbit a little
+    player.rotation = rotateToPoint(app.renderer.plugins.interaction.mouse.global.x, app.renderer.plugins.interaction.mouse.global.y, player.position.x, player.position.y);
+    gun.updatePosition()
+    shield.updatePosition()
+    for (let e in livingEnemys){
+        livingEnemys[e].updatePosition()
+    }
+    handleBullets(bullets,livingEnemys,5)
+    handleBullets(enemyBullets,[player],5)
+   
+
+    // render the container
+    app.renderer.render(app.stage);
+}
+
+function shoot(rotation, startPosition,imgURL,array) {
+    var bullet = new PIXI.Sprite.fromImage(imgURL);
     bullet.anchor.set(0.5)
     bullet.position.x = startPosition.x;
     bullet.position.y = startPosition.y;
     bullet.rotation = rotation;
     app.stage.addChild(bullet);
-    bullets.push(bullet);
+    array.push(bullet);
 }
-function monsterShoot(rotation, startPosition){
+function monsterShoot(rotation, startPosition) {
     var bullet = new PIXI.Sprite.fromImage('./pic/c_bullet.png');
     bullet.anchor.set(0.5)
     bullet.position.x = startPosition.x;
     bullet.position.y = startPosition.y;
     bullet.rotation = rotation;
     app.stage.addChild(bullet);
-    monsterBullets.push(bullet);
+    enemyBullets.push(bullet);
 }
 function rotateToPoint(mx, my, px, py) {
     var self = this;
@@ -106,31 +123,39 @@ function boxesIntersect(a, b) {
     var bb = b.getBounds();
     return ab.x + ab.width > bb.x && ab.x < bb.x + bb.width && ab.y + ab.height > bb.y && ab.y < bb.y + bb.height;
 }
-
-// start animating
-animate();
-app.ticker.add(delta => animate(delta))
-function animate(delta) {
-    //requestAnimationFrame(animate);
-
-    // just for fun, let's rotate mr rabbit a little
-    player.rotation = rotateToPoint(app.renderer.plugins.interaction.mouse.global.x, app.renderer.plugins.interaction.mouse.global.y, player.position.x, player.position.y);
-    
-    shield.updatePosition()
-    monster.updatePosition()
+function handleBullets(bullets,beShoot,bulletSpeed){
     for (var b = bullets.length - 1; b >= 0; b--) {
         bullets[b].position.x += Math.cos(bullets[b].rotation) * bulletSpeed;
         bullets[b].position.y += Math.sin(bullets[b].rotation) * bulletSpeed;
-        if (boxesIntersect(bullets[b], monster)) {
-            console.log('hit')
+        for(var t in beShoot){
+            if (boxesIntersect(bullets[b], beShoot[t])) {
+            }
         }
-        if (bullets[b].position.x > window.innerWidth | bullets[b].position.x < 0 | bullets[b].position.y > window.innerHeight | bullets[b].position.y < 0){
+        if (bullets[b].position.x > window.innerWidth | bullets[b].position.x < 0 | bullets[b].position.y > window.innerHeight | bullets[b].position.y < 0) {
             app.stage.removeChild(bullets[b])
-            bullets.splice(b,1)
+            bullets.splice(b, 1)
         }
-        
-    }
-    // render the container
-    app.renderer.render(app.stage);
-}
 
+    }
+}
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function createCircleEnemy(){
+    var monster = new PIXI.Sprite.fromImage('./pic/circle.png')
+    monster.anchor.set(0.5)
+    var monsterDistance = 300
+    monster.rotation = randomIntFromInterval(0,6)
+    console.log(monster.rotation)
+    monster.updatePosition = () => {
+        monster.rotation += 0.01
+        // 轉一圈就歸零
+        if (monster.rotation > Math.PI * 2) {
+            monster.rotation = 0
+        }
+        monster.x = player.position.x - Math.cos(monster.rotation) * monsterDistance
+        monster.y = player.position.y - Math.sin(monster.rotation) * monsterDistance
+    }
+    app.stage.addChild(monster)
+    livingEnemys.push(monster)
+}
